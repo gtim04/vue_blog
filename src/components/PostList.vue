bcf4bc1f2b1ab0432c47dfdeba1fe47a79000e00
gtim04 <gerald.timtiman@gmail.com>
2023-03-14 15:49:16 +0800

    ending lecture vue3

---
+++ src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+        <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+// import { onMounted, onUnmounted, onUpdated } from 'vue'
+import SinglePost from './SinglePost.vue'
+
+export default {
+    props: ['posts'],
+    components: { SinglePost }
+    // setup(props) {
+    //     onMounted(() => console.log('comp mounted'))
+
+    //     onUnmounted(() => console.log('comp unmounted'))
+
+    //     onUpdated(() => console.log('comp updated'))
+    // }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+        <PostList v-if="showPosts" :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+    <!-- <button @click="showPosts = !showPosts">toggle post</button>
+    <button @click="posts.pop()">delete a post</button> -->
+  </div>
+</template>
+
+<script>
+import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
+
+export default {
+    name: 'Home',
+    components: { PostList },
+    setup() {
+        // const showPosts = ref(true)
+        const showPosts = true
+
+        const { posts, error, load } = getPosts()
+
+        load()
+
+        return { posts , showPosts, error}
+    }
+}
+</script>
+
+<style>
+
+</style>
